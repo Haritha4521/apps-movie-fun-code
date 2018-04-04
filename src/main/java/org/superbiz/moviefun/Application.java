82a171c5d49f94c92207192f1acbae0c62bbfa40
potlamf <potlamf@gmail.com>
2018-04-04 14:28:36 +0800

    Ci added

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.superbiz.moviefun.ActionServlet;
 
 @SpringBootApplication
 public class Application {
